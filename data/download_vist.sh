19b1cfe18ec6677150915bb4e5f7dc8eb6857c5a
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2023-04-05 06:41:16 +0300

    changed wget

--- data/download_vist.sh
+++ data/download_vist.sh
@@ -1,4 +1,4 @@
-wget https://visionandlanguage.net/VIST/json_files/description-in-isolation/DII-with-labels.tar.gz
+wget -nc https://visionandlanguage.net/VIST/json_files/description-in-isolation/DII-with-labels.tar.gz
 tar xvzf DII-with-labels.tar.gz
 mkdir -p vist_images
 python download_vist.py
